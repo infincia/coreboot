288267e64c26ebfe3edd7ad1210e7681e2d60400
Stephen Oliver <steve@infincia.com>
2017-12-10 01:33:22 -0500

    n36l: update config checks to match upstream

--- src/mainboard/hp/n36l/romstage.c
+++ src/mainboard/hp/n36l/romstage.c
@@ -153,7 +153,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 	rs780_early_setup();
 	sb800_early_setup();
 
-#if CONFIG_SET_FIDVID
+#if IS_ENABLED(CONFIG_SET_FIDVID)
 	msr = rdmsr(0xc0010071);
 	printk(BIOS_DEBUG, "\nBegin FIDVID MSR 0xc0010071 0x%08x 0x%08x\n", msr.hi, msr.lo);
 	post_code(0x39);
