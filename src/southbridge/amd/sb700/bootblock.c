ecfedc537463f16b29a9f47d596f6cd36f5f5659
Stephen Oliver <steve@infincia.com>
2017-12-17 21:29:37 -0500

    sb700: don’t enable all LPC IO decodes
    They’re selectively enabled later on

--- src/southbridge/amd/sb700/bootblock.c
+++ src/southbridge/amd/sb700/bootblock.c
@@ -78,12 +78,6 @@ static void sb700_enable_rom(void)
 	pci_io_write_config16(dev, 0x6c, 0x10000 - (CONFIG_COREBOOT_ROMSIZE_KB >> 6));
 	/* Enable LPC ROM range end at 0xffff(ffff). */
 	pci_io_write_config16(dev, 0x6e, 0xffff);
-
-	/* SB700 LPC Bridge 0x44.
-	 * Turn on all LPC IO Port decode enables
-	 */
-	pci_io_write_config32(dev, 0x44, 0xffffffff);
-
 }
 
 static void sb700_configure_rom(void)
