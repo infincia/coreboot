b4afb3073d0d7300f182e445108036336684546d
Stephen Oliver <steve@infincia.com>
2017-12-27 23:10:49 -0500

    n36l: switch to LPC post output
    The N36L has a TPM port that works for port 0x80 post codes very
    early, while any PCI post card will require not only using a PCIe
    device of some sort, but probably additional setup early during boot

--- src/mainboard/hp/n36l/romstage.c
+++ src/mainboard/hp/n36l/romstage.c
@@ -81,7 +81,7 @@ void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 
 		/* enable port80 decoding and southbridge poweron init */
 		sb800_lpc_init();
-		sb800_pci_port80();
+		sb800_lpc_port80();
 	}
 
 	post_code(0x30);
