9387788363aebe6bde633b518410aeb2d292e238
Stephen Oliver <steve@infincia.com>
2017-12-30 01:01:32 -0500

    n36l: disable non-existent DIMM slots

--- src/mainboard/hp/n36l/romstage.c
+++ src/mainboard/hp/n36l/romstage.c
@@ -65,7 +65,7 @@ int spd_read_byte(u32 device, u32 address)
 void cache_as_ram_main(unsigned long bist, unsigned long cpu_init_detectedx)
 {
 	struct sys_info *sysinfo = &sysinfo_car;
-	static const u8 spd_addr[] = {RC00, DIMM0, DIMM2, 0, 0, DIMM1, DIMM3, 0, 0, };
+	static const u8 spd_addr[] = {RC00, DIMM0, 0, 0, 0, DIMM1, 0, 0, 0, };
 	u32 bsp_apicid = 0, val;
 	msr_t msr;
 
